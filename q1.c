e87bec3cb874b44ccf556250df587e503ea79215
kanishka1910 <tyagikanishka2002@gmail.com>
2021-06-13 20:25:27 +0530

    Create q1.c

---
+++ q1.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+void main()
+{
+float a,b,c;
+printf( " ENTER NUMBERS\n");
+scanf ( "%f %f %f" ,&a,&b,&c );
+float avg;
+avg=(a+b+c)/3; /* calculating the average*/
+printf("AVERAGE IS : %f", avg);
+
+}
